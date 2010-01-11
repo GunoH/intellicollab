a798d52934c8a429cd042396fadf76206baf8735
guno <guno@a820628f-bde0-4d17-bc12-966d50119ea5>
2010-01-11 17:00:31 +0000

    IC-14 Dialogen samenvoegen

--- trunk/src/com/quinity/ccollab/intellij/ui/ReviewComboboxRenderer.java
+++ trunk/src/com/quinity/ccollab/intellij/ui/ReviewComboboxRenderer.java
@@ -5,18 +5,21 @@ import com.smartbear.ccollab.datamodel.Review;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
-import java.awt.Component;
+import java.awt.*;
 
 public class ReviewComboboxRenderer extends JLabel implements ListCellRenderer {
+
+	public ReviewComboboxRenderer() {
+		setOpaque(true);
+	}
+
 	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
 		Review review = (Review) value;
 		
 		if (isSelected) {
-			setBackground(list.getSelectionBackground());
-			setForeground(list.getSelectionForeground());
+			setBackground(Color.BLUE);
 		} else {
 			setBackground(list.getBackground());
-			setBackground(list.getForeground());
 		}
 		
 		setText(review.getId() + " " + review.getTitle());
