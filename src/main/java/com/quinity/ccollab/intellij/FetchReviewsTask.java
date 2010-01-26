9527aacd68dfd9238bc590af4e04bf9d33ada352
GunoH <git@guno.nl>
2010-01-26 20:08:10 +0000

    Cleanup

--- src/main/java/com/quinity/ccollab/intellij/FetchReviewsTask.java
+++ src/main/java/com/quinity/ccollab/intellij/FetchReviewsTask.java
@@ -1,8 +1,5 @@
 package com.quinity.ccollab.intellij;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
@@ -11,6 +8,7 @@ import com.intellij.openapi.ui.Messages;
 import com.smartbear.CollabClientException;
 import com.smartbear.ccollab.client.CollabClientConnection;
 import com.smartbear.ccollab.datamodel.Review;
+import org.jetbrains.annotations.NotNull;
 
 public class FetchReviewsTask extends Task.Modal {
 
@@ -29,7 +27,7 @@ public class FetchReviewsTask extends Task.Modal {
 	}
 
 	@Override
-	public void run(ProgressIndicator progressIndicator) {
+	public void run(@NotNull ProgressIndicator progressIndicator) {
 
 		try {
 			progressIndicator.setText(MessageResources.message("progressIndicator.addToReview.retrievingReviews"));
