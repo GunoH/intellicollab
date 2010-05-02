c2cde6a32060942fc36bf44c38ca8f72d3d8f5b7
guno <guno@a820628f-bde0-4d17-bc12-966d50119ea5>
2010-05-02 17:06:11 +0000

    Inloggen en ophalen van gebruikers in aparte tasks ondergebracht

---
+++ trunk/src/main/java/com/quinity/ccollab/intellij/IntelliCcollabAction.java
@@ -0,0 +1,90 @@
+package com.quinity.ccollab.intellij;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.quinity.ccollab.intellij.ui.CreateReviewDialog;
+import com.smartbear.CollabClientException;
+import com.smartbear.beans.ConfigUtils;
+import com.smartbear.beans.IGlobalOptions;
+import com.smartbear.beans.IScmOptions;
+import com.smartbear.ccollab.CommandLineClient;
+import com.smartbear.ccollab.client.CollabClientServerConnectivityException;
+import com.smartbear.ccollab.client.ICollabClientInterface;
+import com.smartbear.ccollab.datamodel.Engine;
+import com.smartbear.ccollab.datamodel.ReviewAccess;
+import com.smartbear.ccollab.datamodel.User;
+import com.smartbear.scm.ScmConfigurationException;
+
+import java.io.IOException;
+
+public abstract class IntelliCcollabAction extends AnAction {
+
+	/**
+	 * Global and SCM options, created by {@link #init(com.intellij.openapi.project.Project)}
+	 */
+	protected static IGlobalOptions globalOptions;
+
+	/**
+	 * SCM options, created by {@link #init(com.intellij.openapi.project.Project)}
+	 */
+	protected static IScmOptions scmOptions;
+
+	/**
+	 * Interface to user for prompting, etc...
+	 * created by {@link #init(com.intellij.openapi.project.Project)}
+	 */
+	protected static ICollabClientInterface clientInterface;
+	
+	/**
+	 * Connection to Code Collaborator server
+	 * created by {@link #init(com.intellij.openapi.project.Project)}
+	 */
+	protected static Engine engine;
+	
+	/**
+	 * Currently logged-in user
+	 * created by {@link #init(com.intellij.openapi.project.Project)}
+	 */
+	protected static User user;
+	
+	protected static void init(Project project) throws CollabClientException, IOException {
+		// If we've already initialized, don't do it again.
+		if ( engine != null ) {
+			return;
+		}
+		
+		//load options from config files
+		com.smartbear.collections.Pair<IGlobalOptions, IScmOptions> configOptions = ConfigUtils.loadConfigFiles();
+		globalOptions = configOptions.getA();
+		scmOptions = configOptions.getB();
+		
+		//initialize client interface
+		clientInterface = new CommandLineClient(globalOptions);
+		
+		//connect to server and log in (throws exception if authentication fails, can't find server, etc...)
+		LoginTask loginTask = new LoginTask(project, globalOptions, clientInterface);
+		loginTask.queue();
+			
+		user = loginTask.getUser();
+		
+		if (user != null) {
+			engine = user.getEngine();
+		}
+	}
+	
+	/**
+	 * Called to clean up a previous call to <code>init()</code>.
+	 * <p/>
+	 * <b>THIS IS CRITICAL</b>.  If you do not close out your <code>CollabClientConnection</code>
+	 * object, data might not be flushed out to the server!
+	 */
+	protected void finished() {
+		if (engine != null) {
+			engine.close(true);
+		}
+	}
+
+}
