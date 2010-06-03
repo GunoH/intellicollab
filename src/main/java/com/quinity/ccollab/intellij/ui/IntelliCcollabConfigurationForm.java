440a9b1f67b7388036ef24ebb53c58e523541422
GunoH <git@guno.nl>
2010-06-03 20:46:35 +0000

    IC-15 - URL naar CC server instelbaar maken - Validatie op invoer

--- src/main/java/com/quinity/ccollab/intellij/ui/IntelliCcollabConfigurationForm.java
+++ src/main/java/com/quinity/ccollab/intellij/ui/IntelliCcollabConfigurationForm.java
@@ -1,11 +1,14 @@
 package com.quinity.ccollab.intellij.ui;
 
 import com.quinity.ccollab.intellij.IntelliCcollabApplicationComponent;
+import org.apache.commons.lang.StringUtils;
 
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class IntelliCcollabConfigurationForm {
 	private JPanel rootComponent;
@@ -23,8 +26,15 @@ public class IntelliCcollabConfigurationForm {
 		urlField.setText(data.getServerURL());
 	}
 
-	public void getData(IntelliCcollabApplicationComponent data) {
-		data.setServerURL(urlField.getText());
+	public void getData(IntelliCcollabApplicationComponent data) throws MalformedURLException {
+		String urlText = urlField.getText();
+		if (StringUtils.isNotEmpty(urlText)) {
+			// Validate the URL.
+			new URL(urlText);
+			data.setServerURL(urlText);
+		} else {
+			data.setServerURL(null);
+		}
 	}
 
 	public boolean isModified(IntelliCcollabApplicationComponent data) {
