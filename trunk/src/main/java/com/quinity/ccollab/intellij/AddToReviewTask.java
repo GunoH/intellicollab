e2b41a4acfb75daaa1e0cbdd4b67477b56bad416
guno <guno@a820628f-bde0-4d17-bc12-966d50119ea5>
2010-06-08 20:41:23 +0000

    IC-35 - Niet-geselecteerde files worden toegevoegd aan review

--- trunk/src/main/java/com/quinity/ccollab/intellij/AddToReviewTask.java
+++ trunk/src/main/java/com/quinity/ccollab/intellij/AddToReviewTask.java
@@ -103,7 +103,7 @@ public class AddToReviewTask extends Task.Backgroundable {
 			Engine engine = AddControlledFileAction.engine;
 			Scm scm = engine.scmByLocalCheckout(scmFile);			// select the SCM system that matches the client configuration
 
-			Changelist changelist = scm.uploadChangeset(changeset, null, "N/A", null, user.getLogin(), 
+			Changelist changelist = scm.uploadChangeset(changeset, null, null, null, user.getLogin(), 
 					"Local changes uploaded from IntelliJ IDEA", new NullProgressMonitor());
 
 			progressIndicator.setText(MessageResources.message("progressIndicator.addToReview.attaching", review.getId()));
