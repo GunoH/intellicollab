c7db4c6618ae548c5da9f5e2781fbd2b04741509
GunoH <git@guno.nl>
2011-07-29 19:35:32 +0000

    Optimized imports

--- src/main/java/com/quinity/ccollab/intellij/FetchMetadataTask.java
+++ src/main/java/com/quinity/ccollab/intellij/FetchMetadataTask.java
@@ -6,7 +6,6 @@ import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.smartbear.ccollab.datamodel.DataModelException;
-import com.smartbear.ccollab.datamodel.GroupDescription;
 import com.smartbear.ccollab.datamodel.MetaDataDescription;
 import com.smartbear.ccollab.datamodel.User;
 import org.jetbrains.annotations.NotNull;
