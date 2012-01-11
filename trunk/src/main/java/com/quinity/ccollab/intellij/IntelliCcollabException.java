4d8af11480b34a53302bc0258f143b3d2a9d1080
guno <guno@a820628f-bde0-4d17-bc12-966d50119ea5>
2012-01-11 18:10:04 +0000

    Removed tabs

--- trunk/src/main/java/com/quinity/ccollab/intellij/IntelliCcollabException.java
+++ trunk/src/main/java/com/quinity/ccollab/intellij/IntelliCcollabException.java
@@ -3,7 +3,7 @@ package com.quinity.ccollab.intellij;
 public class IntelliCcollabException extends Exception {
 
 
-	public IntelliCcollabException(String s) {
-		super(s);
-	}
+    public IntelliCcollabException(String s) {
+        super(s);
+    }
 }
