2aa61e57c9d5d863fafb052dc30228e7cc5833e8
GunoH <github@guno.nl>
2012-01-11 18:10:04 +0000

    Removed tabs

--- src/main/java/com/quinity/ccollab/intellij/ui/IncrementalKeySelManager.java
+++ src/main/java/com/quinity/ccollab/intellij/ui/IncrementalKeySelManager.java
@@ -8,39 +8,39 @@ import java.util.Date;
 
 public abstract class IncrementalKeySelManager implements JComboBox.KeySelectionManager {
  
-	private long lastKeyPressTime = new Date().getTime();
-	private StringBuffer searchString = new StringBuffer("");
+    private long lastKeyPressTime = new Date().getTime();
+    private StringBuffer searchString = new StringBuffer("");
  
-	public int selectionForKey(char aKey, ComboBoxModel aModel) {
-		try {
-			long actKeyPressTime = new Date().getTime();
-	
-			if (lastKeyPressTime + 1000 < actKeyPressTime) {
-				searchString = new StringBuffer("");
-			}
-	
-			if (aKey >= 33 && aKey <= 127) {
-				searchString = searchString.append(aKey);
-			}
-	
-			lastKeyPressTime = actKeyPressTime;
-	
-			for (int i = 0; i < aModel.getSize(); i++) {
-				Object element = aModel.getElementAt(i);
-				if (element == null) {
-					continue;
-				}
-				String listItem = getDisplayedText(element);
-	
-				if (listItem.startsWith(searchString.toString().toUpperCase())) {
-					return i;
-				}
-			}
-			return -1;
-		} catch (Exception ex) {
-			return -1;
-		}
-	}
+    public int selectionForKey(char aKey, ComboBoxModel aModel) {
+        try {
+            long actKeyPressTime = new Date().getTime();
 
-	protected abstract String getDisplayedText(Object object);
+            if (lastKeyPressTime + 1000 < actKeyPressTime) {
+                searchString = new StringBuffer("");
+            }
+
+            if (aKey >= 33 && aKey <= 127) {
+                searchString = searchString.append(aKey);
+            }
+
+            lastKeyPressTime = actKeyPressTime;
+
+            for (int i = 0; i < aModel.getSize(); i++) {
+                Object element = aModel.getElementAt(i);
+                if (element == null) {
+                    continue;
+                }
+                String listItem = getDisplayedText(element);
+
+                if (listItem.startsWith(searchString.toString().toUpperCase())) {
+                    return i;
+                }
+            }
+            return -1;
+        } catch (Exception ex) {
+            return -1;
+        }
+    }
+
+    protected abstract String getDisplayedText(Object object);
 }
