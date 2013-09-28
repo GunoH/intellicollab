c080b10154736ae5de4b7840ba9590cfb0bcdd68
guno <guno@a820628f-bde0-4d17-bc12-966d50119ea5>
2013-09-28 10:03:04 +0000

    Tagged v0.17

---
+++ tags/0_17/src/main/java/nl/guno/ccollab/intellij/IntelliCcollabAction.java
@@ -0,0 +1,131 @@
+package nl.guno.ccollab.intellij;
+
+import java.io.IOException;
+
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.intellij.notification.NotificationListener;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.MessageType;
+import com.smartbear.CollabClientException;
+import com.smartbear.beans.ConfigUtils;
+import com.smartbear.beans.IGlobalOptions;
+import com.smartbear.beans.IScmOptions;
+import com.smartbear.ccollab.CommandLineClient;
+import com.smartbear.ccollab.client.ICollabClientInterface;
+import com.smartbear.ccollab.datamodel.Engine;
+import com.smartbear.ccollab.datamodel.User;
+import com.smartbear.collections.Pair;
+import nl.guno.ccollab.intellij.ui.Notification;
+
+abstract class IntelliCcollabAction extends AnAction {
+
+    /**
+     * SCM options, created by {@link #init(com.intellij.openapi.project.Project)}
+     */
+    static IScmOptions scmOptions;
+
+    /**
+     * Connection to Code Collaborator server
+     * created by {@link #init(com.intellij.openapi.project.Project)}
+     */
+    static Engine engine;
+
+    /**
+     * Currently logged-in user
+     * created by {@link #init(com.intellij.openapi.project.Project)}
+     */
+    static User user;
+
+    private static final IntelliCcollabApplicationComponent component =
+            ApplicationManager.getApplication().getComponent(IntelliCcollabApplicationComponent.class);
+
+    static void init(final Project project) throws CollabClientException, IOException {
+        // If we've already initialized, don't do it again.
+        if ( engine != null ) {
+            return;
+        }
+
+        //load options from config files
+        Pair<IGlobalOptions, IScmOptions> configOptions = ConfigUtils.loadConfigFiles();
+        IntelliCcollabGlobalOptions globalOptions = new IntelliCcollabGlobalOptions(configOptions.getA());
+
+        if (globalOptions.settingsIncomplete()) {
+            new Notification(project, MessageResources.message("configuration.error.mandatorySettingsMissing.text"),
+                    MessageType.ERROR).setHyperlinkListener(new HyperlinkListener() {
+                @Override
+                public void hyperlinkUpdate(HyperlinkEvent e) {
+                    if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                        openSettings(project);
+                    }
+                }
+            }).setNotificationListener(new NotificationListener() {
+                @Override
+                public void hyperlinkUpdate(@NotNull com.intellij.notification.Notification notification, @NotNull HyperlinkEvent hyperlinkEvent) {
+                    openSettings(project);
+                }
+            }).showBalloon().addToEventLog();
+            return;
+        }
+
+        scmOptions = configOptions.getB();
+
+        //initialize client interface
+        ICollabClientInterface clientInterface = new CommandLineClient(globalOptions);
+
+        //connect to server and log in (throws exception if authentication fails, can't find server, etc...)
+        LoginTask loginTask = new LoginTask(project, globalOptions, clientInterface);
+        loginTask.queue();
+
+        if (!loginTask.success()) {
+            if (loginTask.authenticationErrorOccured()) {
+                new Notification(project, MessageResources.message("task.login.authenticationError.text"),
+                        MessageType.ERROR).setHyperlinkListener(new HyperlinkListener() {
+                    @Override
+                    public void hyperlinkUpdate(HyperlinkEvent e) {
+                        if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                            openSettings(project);
+                        }
+                    }
+                }).setNotificationListener(new NotificationListener() {
+                    @Override
+                    public void hyperlinkUpdate(@NotNull com.intellij.notification.Notification notification,
+                                                @NotNull HyperlinkEvent hyperlinkEvent) {
+                        openSettings(project);
+                    }
+                }).showBalloon().addToEventLog();
+            }
+
+            return;
+        }
+        
+        user = loginTask.getUser();
+
+        if (user != null) {
+            engine = user.getEngine();
+        }
+    }
+
+    private static void openSettings(Project project) {
+        ShowSettingsUtil.getInstance().editConfigurable(project, component);
+    }
+
+    /**
+     * Called to clean up a previous call to {@link #init(Project)}.
+     * <p/>
+     * <b>THIS IS CRITICAL</b>.  If you do not close out your {@code CollabClientConnection}
+     * object, data might not be flushed out to the server!
+     */
+    void finished() {
+        if (engine != null) {
+            engine.close(true);
+        }
+    }
+
+}
---
+++ tags/0_17/src/main/java/nl/guno/ccollab/intellij/IntelliCcollabGlobalOptions.java
@@ -0,0 +1,88 @@
+package nl.guno.ccollab.intellij;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.smartbear.beans.IGlobalOptions;
+
+public class IntelliCcollabGlobalOptions implements IGlobalOptions {
+
+    private final IGlobalOptions wrappedOptions;
+
+    private final IntelliCcollabApplicationComponent component =
+            ApplicationManager.getApplication().getComponent(IntelliCcollabApplicationComponent.class);
+
+    public IntelliCcollabGlobalOptions(IGlobalOptions wrappedOptions) {
+        this.wrappedOptions = wrappedOptions;
+
+    }
+
+    /**
+     * Indicates if any of the mandatory settings are missing.
+     * @return {@code true} if any of the mandatory settings are missing, {@code false} otherwise.
+     */
+    public boolean settingsIncomplete() {
+        return StringUtils.isEmpty(component.getServerURL())
+                || StringUtils.isEmpty(component.getUsername())
+                || StringUtils.isEmpty(component.getPassword());
+    }
+
+    public URL getUrl() {
+        try {
+            return new URL(component.getServerURL());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Invalid URL:" + component.getServerURL());
+        }
+    }
+
+    public String getServerProxyHost() {
+        return component.getServerProxyHost();
+    }
+
+    public String getServerProxyPort() {
+        return component.getServerProxyPort();
+    }
+
+    public String getUser() {
+        return component.getUsername();
+    }
+
+    public String getPassword() {
+        return component.getPassword();
+    }
+
+    public Boolean isNoBrowser() {
+        return wrappedOptions.isNoBrowser();
+    }
+
+    public Boolean isNonInteractive() {
+        return wrappedOptions.isNonInteractive();
+    }
+
+    public Boolean isQuiet() {
+        return wrappedOptions.isQuiet();
+    }
+
+    public String getEditor() {
+        return wrappedOptions.getEditor();
+    }
+
+    public Boolean isEditorPrompt() {
+        return wrappedOptions.isEditorPrompt();
+    }
+
+    public Boolean isPauseOnError() {
+        return wrappedOptions.isPauseOnError();
+    }
+
+    public String getBrowser() {
+        return wrappedOptions.getBrowser();
+    }
+
+    public Boolean isForceNewBrowser() {
+        return wrappedOptions.isForceNewBrowser();
+    }
+}
