78c4f3a2ef8a26474fc647ea730c3718cfd1cd60
GunoH <git@guno.nl>
2018-04-04 16:51:12 +0200

    Build against 2018.1.

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,6 +3,6 @@
 object BuildConfig {
     const val kotlinVersion = "1.2.30"
     const val gradleIntellijPluginVersion = "0.3.1"
-    const val intellijVersion = "IU-LATEST-EAP-SNAPSHOT"
+    const val intellijVersion = "IU-2018.1"
     const val pluginVersion = "2.0"
 }
