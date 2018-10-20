ce71f70ab40f45297b83fcc12d1baefb61b4a7ab
GunoH <git@guno.nl>
2018-10-20 22:48:10 +0200

    Upgrade Gradle Intellij plugin to 0.3.12.

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -2,7 +2,7 @@
 
 object BuildConfig {
     const val kotlinVersion = "1.2.71"
-    const val gradleIntellijPluginVersion = "0.3.1"
+    const val gradleIntellijPluginVersion = "0.3.12"
     const val intellijVersion = "IU-2018.1"
     const val pluginVersion = "2.0"
 }
