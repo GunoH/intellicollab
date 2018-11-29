eadebf99911b3c1cf52082a97b6a5f4028de92aa
GunoH <git@guno.nl>
2018-11-29 15:36:05 +0100

    Version 2.1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,5 +4,5 @@ object BuildConfig {
     const val kotlinVersion = "1.3.10"
     const val gradleIntellijPluginVersion = "0.3.12"
     const val intellijVersion = "IU-2018.1"
-    const val pluginVersion = "2.0"
+    const val pluginVersion = "2.1"
 }
