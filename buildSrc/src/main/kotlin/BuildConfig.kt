6adb200b92a4af4c43d75e657c4677dfeb036898
GunoH <git@guno.nl>
2018-11-29 15:25:40 +0100

    Upgrade Kotlin runtime to 1.3.10.

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,7 +1,7 @@
 @file:Suppress("unused")
 
 object BuildConfig {
-    const val kotlinVersion = "1.2.71"
+    const val kotlinVersion = "1.3.10"
     const val gradleIntellijPluginVersion = "0.3.12"
     const val intellijVersion = "IU-2018.1"
     const val pluginVersion = "2.0"
