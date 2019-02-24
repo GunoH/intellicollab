b43b57311e69ced36ee8b136456577822c666698
GunoH <git@guno.nl>
2019-02-24 04:41:45 +0100

    Upgrade Gradle Intellij plugin to 0.4.2.

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -2,7 +2,7 @@
 
 object BuildConfig {
     const val kotlinVersion = "1.3.10"
-    const val gradleIntellijPluginVersion = "0.3.12"
+    const val gradleIntellijPluginVersion = "0.4.2"
     const val intellijVersion = "IU-2018.1"
     const val pluginVersion = "2.1"
 }
